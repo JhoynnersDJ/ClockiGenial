d8d8a14552687b183fe91e238661dc5bf85e0e97
Lacrita24 <jhoy16ps4@gmail.com>
2023-10-11 15:23:58 -0400

    Modificaciones correo de registro

--- Clocki-back/app.js
+++ Clocki-back/app.js
@@ -4,8 +4,9 @@ const bodyParser = require('body-parser');
 const { initializeApp } = require('firebase/app');
 const { getFirestore, collection, getDocs, query, where, addDoc, doc,getDoc} = require('firebase/firestore');
 const { sign } = require('jsonwebtoken');
-
 const { db, firebase } = require('./database/firebase');
+const nodemailer = require('nodemailer');
+const emailConfig = require('./emailConfig'); // Importa la configuración
 
 const secretToken = "Hl*2l3c#Tl4&Rb2Dp!5s";
 
@@ -39,13 +40,82 @@ app.post('/registro', async (req, res) => {
         // Guarda el nuevo usuario en Firestore
         const usuariosRef = collection(db, 'usuarios'); // Utiliza collection aquí
         await addDoc(usuariosRef, usuarioNuevo);
+        // Envía un correo al usuario
+        const transporter = nodemailer.createTransport({
+            "service": "gmail",
+            "auth": {
+              "type": "OAuth2",
+              "user": "omnitetgroup01@gmail.com",
+              "clientId": "753006295934-a5s4k797r91oqeo43j7srr548v715a78.apps.googleusercontent.com",
+              "clientSecret": "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",
+              "refreshToken": "1//04SN02pSPtZPUCgYIARAAGAQSNwF-L9IratxmpXieduedcXWRCIBDJLv9GLM1dgsWRzndFAwiujWnMqRasz20blw9hrput7ZB0LY"
+            }
+          });
 
+const htmlContent = `
+<html>
+  <head>
+    <style>
+      /* Estilos CSS personalizados */
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f2f2f2; /* Cambio de color de fondo */
+      }
+      .container {
+        max-width: 600px;
+        margin: 0 auto;
+        padding: 20px;
+        background-color: #fff;
+        border-radius: 5px;
+        box-shadow: 0 0 10px #f2f2f2;
+      }
+      h1 {
+        font-size: 24px;
+        margin-bottom: 10px; /* Espacio adicional después del título */
+      }
+      .omnitime {
+        color: #510DB6; /* Cambio de color solo para "OmniTime" */
+      }
+      p {
+        color: #666;
+        font-size: 14px; /* Tamaño de fuente reducido para el correo */
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Bienvenido a <span class="omnitime">OmniTime</span> falta modificar</h1>
+      <p>¡Gracias por registrarte en nuestro servicio! Esperamos que disfrutes de nuestra plataforma.</p>
+      <p>Tu dirección de correo es: ${email}</p>
+    </div>
+  </body>
+</html>
+`;
+
+
+        // Configura el correo electrónico de verificación
+    const mailOptions = {
+        from: 'omnitetgroup01@gmail.com', // Cambia esto por tu dirección de correo
+        to: email,
+        subject: 'Verifica tu correo electrónico',
+        html: htmlContent,
+      };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Error al enviar el correo:', error);
+            } else {
+                console.log('Correo enviado:', info.response);
+            }
+        });
         res.status(200).json({ message: 'Usuario guardado con éxito en Firestore' });
     } catch (error) {
         console.error('Error al guardar el usuario en Firestore:', error);
         res.status(500).json({ error: 'Ocurrió un error al guardar el usuario' });
-    }
-});
+        }
+
+        
+    });
 
 // Endpoint para iniciar sesión
 app.post('/login', async (req, res) => {
---
+++ Clocki-back/emailConfig.js
@@ -0,0 +1,13 @@
+const emailConfig = {
+  "service": "gmail",
+  "auth": {
+    "type": "OAuth2",
+    "user": "omnitetgroup01@gmail.com",
+    "clientId": "753006295934-a5s4k797r91oqeo43j7srr548v715a78.apps.googleusercontent.com",
+    "clientSecret": "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",
+    "refreshToken": "1//04SN02pSPtZPUCgYIARAAGAQSNwF-L9IratxmpXieduedcXWRCIBDJLv9GLM1dgsWRzndFAwiujWnMqRasz20blw9h"
+  }
+};
+
+// Exporta la configuración para que otros archivos puedan usarla
+module.exports = emailConfig;
