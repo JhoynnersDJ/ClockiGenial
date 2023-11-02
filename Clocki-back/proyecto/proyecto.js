40749b7dc91b8308d2a1ab2a043ebe90e2dad312
Lacrita24 <jhoy16ps4@gmail.com>
2023-11-02 12:09:36 -0400

    Incluido respuesta en las actividades por usuario, la categoria de proyecto

--- Clocki-back/proyecto/proyecto.js
+++ Clocki-back/proyecto/proyecto.js
@@ -6,7 +6,7 @@ const {db} = require ('../database/firebase');
 //registrar un proyecto
 router.post('/registro-proyecto', async (req, res) => {
     try {
-      const { nombre_proyecto, id_cliente, id_usuario } = req.body;
+      const { nombre_proyecto, id_cliente, id_usuario, descripcion, categoria } = req.body;
   
       // Verifica si la actividad existe antes de continuar
       const usuarioRef = doc(db, 'usuario', id_usuario);
@@ -19,6 +19,8 @@ router.post('/registro-proyecto', async (req, res) => {
         nombre_proyecto: nombre_proyecto,
         cliente: id_cliente ? doc(db, 'clientes', id_cliente) : null, // Referencia al cliente (puede ser nulo)
         usuario: usuarioRef, // Referencia al usuario (obligatorio)
+        descripcion: descripcion,
+        categoria: categoria,
       };
   
       // Guarda el proyecto en Firestore
