3abd2e0bcc8ba99ad6720903018cdfe5fb697fba
Lacrita24 <jhoy16ps4@gmail.com>
2023-11-07 10:43:07 -0400

    Modificacion de clientes (pido el id_suuario) y nueva creacion de lista de clientes

--- Clocki-back/cliente/cliente.js
+++ Clocki-back/cliente/cliente.js
@@ -5,25 +5,26 @@ const {db} = require ('../database/firebase');
 
 // Ruta para registrar un cliente
 router.post('/registro-cliente', async (req, res) => {
-    try {
-      const { nombre_cliente} = req.body;
-  
-      // Crea un nuevo documento en la colección "clientes"
-      const clientesRef = collection(db, 'clientes');
-  
-      // Define los datos del cliente
-      const clienteData = {
-        nombre_cliente: nombre_cliente,
-      };
-  
-      // Guarda el cliente en Firestore
-      const nuevoClienteRef = await addDoc(clientesRef, clienteData);
-  
-      res.status(201).json({ message: 'Cliente registrado con éxito', clienteId: nuevoClienteRef.id });
-    } catch (error) {
-      console.error('Error al registrar cliente:', error);
-      res.status(500).json({ error: 'Ocurrió un error al registrar cliente' });
-    }
-  });
+  try {
+    const { nombre_cliente, id_usuario } = req.body;
+
+    // Crea un nuevo documento en la colección "clientes"
+    const clientesRef = collection(db, 'clientes');
+
+    // Define los datos del cliente, incluyendo la referencia al usuario
+    const clienteData = {
+      nombre_cliente: nombre_cliente,
+      usuario: doc(db, 'usuarios', id_usuario), // Referencia al usuario
+    };
+
+    // Guarda el cliente en Firestore
+    const nuevoClienteRef = await addDoc(clientesRef, clienteData);
+
+    res.status(201).json({ message: 'Cliente registrado con éxito', clienteId: nuevoClienteRef.id });
+  } catch (error) {
+    console.error('Error al registrar cliente:', error);
+    res.status(500).json({ error: 'Ocurrió un error al registrar cliente' });
+  }
+});
   
   module.exports = router;
