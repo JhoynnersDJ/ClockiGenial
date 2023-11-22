379849f324ac81ac33f1443e9ca3e024e0539d24
Lacrita24 <jhoy16ps4@gmail.com>
2023-11-22 12:54:19 -0400

    Añadido el reporte mensual

--- BackMongo/reportes/reportes.js
+++ BackMongo/reportes/reportes.js
@@ -24,9 +24,6 @@ router.post('/informe-diario', async (req, res) => {
       })
       .exec();
 
-    // Log de proyectos antes de calcular la ganancia total
-    console.log('Proyectos para el cálculo de ganancia total:', actividadesDiarias.map(actividad => actividad.proyecto));
-
     // Obtener información del usuario
     const usuario = await Usuario.findById(id_usuario).select('nombre apellido email').exec();
 
@@ -49,14 +46,14 @@ router.post('/informe-semanal/', async (req, res) => {
     // Convertir la cadena de fecha de inicio a un objeto Date
     const fechaInicio = new Date(fecha_inicio);
 
-// Calcula las fechas de inicio y fin de la semana que contiene la fecha de inicio
-const primerDiaSemana = new Date(fechaInicio);
-const diaSemana = fechaInicio.getDay();
-const diferenciaDias = diaSemana >= 1 ? diaSemana - 0 : 6; // Si es domingo, restamos 1 día; de lo contrario, restamos la cantidad de días transcurridos desde el lunes
-primerDiaSemana.setDate(fechaInicio.getDate() - diferenciaDias);
+    // Calcula las fechas de inicio y fin de la semana que contiene la fecha de inicio
+    const primerDiaSemana = new Date(fechaInicio);
+    const diaSemana = fechaInicio.getDay();
+    const diferenciaDias = diaSemana >= 1 ? diaSemana - 0 : 6; // Si es domingo, restamos 1 día; de lo contrario, restamos la cantidad de días transcurridos desde el lunes
+    primerDiaSemana.setDate(fechaInicio.getDate() - diferenciaDias);
 
-const ultimoDiaSemana = new Date(primerDiaSemana);
-ultimoDiaSemana.setDate(primerDiaSemana.getDate() + 6);
+    const ultimoDiaSemana = new Date(primerDiaSemana);
+    ultimoDiaSemana.setDate(primerDiaSemana.getDate() + 6);
 
     // Realiza una consulta para obtener las actividades en el rango de fechas y del usuario específico
     const actividadesSemana = await Actividad.find({
@@ -78,8 +75,6 @@ ultimoDiaSemana.setDate(primerDiaSemana.getDate() + 6);
       })
       .exec();
 
-    // Log de proyectos antes de calcular la ganancia total
-    console.log('Proyectos para el cálculo de ganancia total:', actividadesSemana.map(actividad => actividad.proyecto));
 
     // Calcular la ganancia por proyecto y los ingresos totales
     const gananciaPorProyecto = actividadesSemana.reduce((result, actividad) => {
@@ -106,6 +101,9 @@ ultimoDiaSemana.setDate(primerDiaSemana.getDate() + 6);
     // Redondear los ingresos totales a dos decimales
     const ingresosTotalesDosDecimales = parseFloat(ingresosTotales.toFixed(2));
 
+    // Obtener información del usuario
+    const usuario = await Usuario.findById(id_usuario).select('nombre apellido email').exec();
+
     // Resto del código para procesar y enviar la respuesta
 
     res.status(200).json({
@@ -113,6 +111,11 @@ ultimoDiaSemana.setDate(primerDiaSemana.getDate() + 6);
         rango_fechas: {
           inicio: primerDiaSemana.toISOString().split('T')[0],
           fin: ultimoDiaSemana.toISOString().split('T')[0],
+        },
+        usuario: {
+          nombre: usuario.nombre,
+          apellido: usuario.apellido,
+          email: usuario.email,
         },
         actividades: actividadesSemana.map(actividad => ({
           id_actividad: actividad._id,
@@ -127,6 +130,97 @@ ultimoDiaSemana.setDate(primerDiaSemana.getDate() + 6);
   } catch (error) {
     console.error('Error al obtener informe semanal:', error);
     res.status(500).json({ error: 'Ocurrió un error al obtener el informe semanal' });
+  }
+});
+
+// Endpoint para obtener informe mensual
+router.post('/informe-mensual', async (req, res) => {
+  try {
+    const { mes, año, id_usuario } = req.body;
+
+    // Verificar que mes y año sean valores válidos
+    if (mes < 1 || mes > 12 || año < 1900) {
+      return res.status(400).json({ error: 'Mes o año inválido' });
+    }
+
+    // Calcular las fechas de inicio y fin del mes
+    const primerDiaMes = new Date(año, mes - 1, 1);
+    const ultimoDiaMes = new Date(año, mes, 0);
+
+    // Realizar una consulta para obtener las actividades en el rango de fechas y del usuario específico
+    const actividadesMes = await Actividad.find({
+      fecha_registro: {
+        $gte: primerDiaMes.toISOString().split('T')[0], // Convertir a formato YYYY-MM-DD
+        $lte: ultimoDiaMes.toISOString().split('T')[0],
+      },
+      usuario: id_usuario, // Agregar esta condición para filtrar por usuario
+    })
+      .populate({
+        path: 'proyecto',
+        model: 'Proyecto',
+        populate: {
+          path: 'cliente',
+          model: 'Cliente',
+          select: 'nombre_cliente',
+        },
+        select: 'nombre_proyecto cliente tarifa_total',
+      })
+      .exec();
+
+    // Calcular la ganancia por proyecto y los ingresos totales
+    const gananciaPorProyecto = actividadesMes.reduce((result, actividad) => {
+      const proyectoId = actividad.proyecto ? actividad.proyecto._id.toString() : null; // Convertir a cadena para comparar
+      if (proyectoId) {
+        if (!result[proyectoId]) {
+          result[proyectoId] = {
+            proyecto: actividad.proyecto.nombre_proyecto,
+            gananciaTotal: 0,
+          };
+        }
+        result[proyectoId].gananciaTotal += actividad.total_tarifa || 0;
+        result[proyectoId].gananciaTotal = parseFloat(result[proyectoId].gananciaTotal.toFixed(2)); // Redondear a dos decimales
+      }
+      return result;
+    }, {});
+
+    // Calcular los ingresos totales del usuario
+    const ingresosTotales = actividadesMes.reduce((total, actividad) => {
+      total += actividad.total_tarifa || 0;
+      return total;
+    }, 0);
+
+    // Redondear los ingresos totales a dos decimales
+    const ingresosTotalesDosDecimales = parseFloat(ingresosTotales.toFixed(2));
+
+    // Obtener información del usuario
+    const usuario = await Usuario.findById(id_usuario).select('nombre apellido email').exec();
+
+    // Resto del código para procesar y enviar la respuesta
+
+    res.status(200).json({
+      informeMensual: {
+        rango_fechas: {
+          inicio: primerDiaMes.toISOString().split('T')[0],
+          fin: ultimoDiaMes.toISOString().split('T')[0],
+        },
+        usuario: {
+          nombre: usuario.nombre,
+          apellido: usuario.apellido,
+          email: usuario.email,
+        },
+        actividades: actividadesMes.map(actividad => ({
+          id_actividad: actividad._id,
+          nombre_actividad: actividad.nombre_actividad,
+          duracion_total: actividad.duracion_total,
+          // Otros campos que desees incluir
+        })),
+        gananciaPorProyecto,
+        ingresosTotales: ingresosTotalesDosDecimales,
+      },
+    });
+  } catch (error) {
+    console.error('Error al obtener informe mensual:', error);
+    res.status(500).json({ error: 'Ocurrió un error al obtener el informe mensual' });
   }
 });
 
